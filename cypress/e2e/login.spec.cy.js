6dd03fedbcbfbcd5dc98550b990936a575bc1495
brunabeza <bruna.beza@gmail.com>
2024-09-19 21:59:37 -0300

    primeiro teste

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,5 +1,8 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Login', () => {
+  it('Should be able to login successfully', () => {
+    cy.visit('http://localhost:3000/signin')
+    cy.get('#username').type('Arvilla_Hegmann')
+    cy.get('#password').type('s3cret')
+    cy.get('[data-test="signin-submit"]').click()
   })
 })
