fc07db8de591a2e5d82725fd98efdcc4efd24180
brunabeza <bruna.beza@gmail.com>
2025-03-26 21:20:15 -0300

    yml

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,43 @@
 const { defineConfig } = require("cypress")
-const webpack = require('@cypress/webpack-preprocessor')
+const { exec } = require('child_process')
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',  
+
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Configurar o reporter Mochawesome
+      on('after:run', () => {
+        // Mesclar arquivos JSON de resultados dos testes
+        exec('npx mochawesome-merge cypress/results/*.json > cypress/results/all-tests.json', (err, stdout, stderr) => {
+          if (err) {
+            console.error('Erro ao mesclar os relatórios:', stderr);
+          } else {
+            console.log(stdout);
+
+            // Gerar o relatório HTML a partir do JSON mesclado
+            exec('npx mochawesome-report-generator cypress/results/all-tests.json', (err, stdout, stderr) => {
+              if (err) {
+                console.error('Erro ao gerar o relatório:', stderr);
+              } else {
+                console.log(stdout);
+              }
+            });
+          }
+        });
+      });
+
+      return config;
+    },
+
+    // Definir o reporter como Mochawesome
+    reporter: 'mochawesome',
+    
+    reporterOptions: {
+      reportDir: 'cypress/results',  // Onde os relatórios serão salvos
+      overwrite: true,               // Sobrescrever os relatórios antigos
+      html: true,                    // Gerar relatório em HTML
+      json: true                     // Gerar relatório em JSON
     },
   },
-})
+});
